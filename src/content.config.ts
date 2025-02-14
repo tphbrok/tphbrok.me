703a697bb20dcaedba818b3cba943fc443263fa6
Thomas Brok <tphbrok@gmail.com>
2025-02-14 11:56:44 +0100

    Add tags pages, add series pages (manual for now), fix header image width

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
     loader: glob({ base: "./src/content/blog", pattern: "*.mdx" }),
     schema: z.object({
-        categories: z.array(z.string()).max(3),
+        tags: z.array(z.string()).max(3),
         dateCreated: z.coerce.date(),
         description: z.string(),
         title: z.string(),
